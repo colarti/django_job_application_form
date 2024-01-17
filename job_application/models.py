e2694371c3ff7d508c91a14bac721a1c5d19455e
cmolarti <cmolarti@gmail.com>
2024-01-17 16:21:46 -0500

    filled in models.py, created a migration, then updated the db.sqlite3 database

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Form(models.Model):
+    fname = models.CharField(max_length=80)
+    lname = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f'{self.fname} {self.lname} - {self.email} <> {self.date} [{self.occupation}]'
